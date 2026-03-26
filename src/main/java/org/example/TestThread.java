877bffdfb360cee699b5d40b958209760f3b901b
S0umyajit <soumyajit2k18@gmail.com>
2026-03-27 02:09:45 +0530

    Threads

--- src/main/java/org/example/TestThread.java
+++ src/main/java/org/example/TestThread.java
@@ -1,6 +1,7 @@
 package org.example;
 
 //Using Thread class we are trying to create Thread
+//Perform single task from single thread
 public class TestThread extends Thread {
     public void run(){
         System.out.println("Thread start");
