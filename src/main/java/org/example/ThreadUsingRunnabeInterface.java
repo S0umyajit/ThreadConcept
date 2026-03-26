dfbb646194b5a50a090802bf2558d93beca89653
S0umyajit <soumyajit2k18@gmail.com>
2026-03-27 01:27:49 +0530

    This is for my Thread Practice

--- src/main/java/org/example/ThreadUsingRunnabeInterface.java
+++ src/main/java/org/example/ThreadUsingRunnabeInterface.java
@@ -1,13 +1,18 @@
 package org.example;
 
 public class ThreadUsingRunnabeInterface implements Runnable{
+    int val;
+    ThreadUsingRunnabeInterface(int val){
+        this.val=val;
+    }
+
     @Override
     public void run() {
-        System.out.println("Hello From Thread");
+        System.out.println("Hello From Thread "+val);
     }
 
     public static void main(String[] args) {
-        ThreadUsingRunnabeInterface th=new ThreadUsingRunnabeInterface();
+        ThreadUsingRunnabeInterface th=new ThreadUsingRunnabeInterface(12);
         Thread tr=new Thread(th);
         tr.start();
     }
