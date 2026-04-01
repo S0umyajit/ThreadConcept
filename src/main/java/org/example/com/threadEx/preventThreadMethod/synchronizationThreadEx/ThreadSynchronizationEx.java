1e7364c1a207c83c05ff7a1d882caa04201ab50d
S0umyajit <soumyajit2k18@gmail.com>
2026-04-02 01:45:08 +0530

    Synchronization example used synchronized keyword and make the method synchronized

--- src/main/java/org/example/com/threadEx/preventThreadMethod/synchronizationThreadEx/ThreadSynchronizationEx.java
+++ src/main/java/org/example/com/threadEx/preventThreadMethod/synchronizationThreadEx/ThreadSynchronizationEx.java
@@ -3,7 +3,12 @@ package org.example.com.threadEx.preventThreadMethod.synchronizationThreadEx;
 class BookTheaterSeat{
     int totalSeats =10;
 
-    void bookSeat(int seats){
+    //synchronized keyword will make the method as synchronized method
+    //now this will work fine if there is multiple thread
+    //cause a lock concept is coming here
+    //when one thread is using one shared method then thread will aquire lock
+    //other thread has to wait until that thread release lock
+   synchronized void bookSeat(int seats){
         if(totalSeats>=seats){
             System.out.println("Seats Booked Successfully "+seats);
             totalSeats-=seats;
