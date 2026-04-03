08426bc24ca1abf39946dd1883f03c3f1ba9bf13
S0umyajit <soumyajit2k18@gmail.com>
2026-04-04 03:11:06 +0530

    UdemyThreadP

--- src/main/java/com/ex/staticSynchronizationex/Abc.java
+++ src/main/java/com/ex/staticSynchronizationex/Abc.java
@@ -7,9 +7,19 @@ public class Abc {
             public void run() {
                 System.out.println("I am in: "+Thread.currentThread().getName());
                 System.out.println("Child: "+Thread.currentThread().getPriority());
+                System.out.println(Thread.currentThread().getName());
+                throw new RuntimeException("Intentational Exception");
             }
         });
         thread.setPriority(Thread.MAX_PRIORITY);
+        thread.setName("MyThread");
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A error happened in thread: "+t.getName()
+                                                    +"and error message is: "+e.getMessage());
+            }
+        });
         thread.start();
         System.out.println(Thread.currentThread().getName());
     }
